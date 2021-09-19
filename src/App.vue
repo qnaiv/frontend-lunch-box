ccb9cee05891dd78e3d824ad4b6011ed0c5864b4
qnaiv <43468715+qnaiv@users.noreply.github.com>
2021-09-19 13:19:44 +0000

    init twitterトレンド

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="hero">
+    <img src="lunchbox-logo.png" />
+    frontend-lunch-box
+  </div>
+  <div id="gallery">
+    <div class="gallery-item">
+      <div class="header">
+        <div class="title">Twitterトレンド欄</div>
+        <div class="created-date">2021-09-19</div>
+      </div>
+      <twitter-trend />
+    </div>
+  </div>
+</template>
+
+<script>
+import TwitterTrend from './components/TwitterTrend/TwitterTrend.vue'
+export default {
+  name: 'App',
+  components: {
+    TwitterTrend
+  }
+}
+</script>
+
+<style scoped>
+body{
+  background-color: whitesmoke;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#hero {
+  font-size: 1.5em;
+  font-weight: 200;
+  text-align: left;
+  padding: 50px;
+  font-family: "M PLUS Rounded 1c";
+}
+#hero img {
+  height: 1.5em;
+  vertical-align: middle;
+  /* margin-right: 10px; */
+}
+#gallery {
+  display: flex;
+  padding: 0 2%;
+}
+
+.gallery-item{
+  background-color: white;
+  border-radius: 10px;
+  padding: 15px;
+  filter: drop-shadow(3px 3px 10px #cccccc)
+}
+
+.gallery-item .header{
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.gallery-item .header .title{
+  flex: 1;
+}
+.gallery-item .header .created-date{
+    text-align: right;
+    font-size: 0.8em;
+    color: gray;
+}
+
+@media (max-width: 40.0rem) {
+  #hero {
+    font-size: 1.5em;
+    text-align: center;
+    padding: 50px 0px;
+  }
+  #hero img {
+    height: 1.5em;
+    display: block;
+    margin: 0 auto;
+  }
+}
+</style>
---
+++ src/components/TwitterTrend/TwitterTrend.vue
@@ -0,0 +1,126 @@
+<template>
+  <div id="twitter-trend-container">
+    <h2>
+        いまどうしてる？
+    </h2>
+    <div class="trend">
+        <div class="content">
+            <span class="category">
+                国際ニュース·ライブ
+            </span>
+            <span class="title">
+                クルードラゴン 世界初民間人だけの宇宙旅行終え地球に帰還
+            </span>
+        </div>
+        <div class="thumbnail">
+            <img src="./trend-image.jpg">
+        </div>
+    </div>
+    <div class="trend">
+        <div class="content">
+            <span class="category">
+                ゲーム·トレンド
+            </span>
+            <span class="title">
+                #ウデマエX到達までにしてた事
+            </span>
+            <span class="additional">
+                4,156件のツイート
+            </span>
+        </div>
+        <div class="option">
+            ...
+        </div>
+    </div>
+    <div class="trend">
+        <div class="content">
+            <span class="category">
+            日本のトレンド
+            </span>
+            <span class="title">
+                配信ライブ中
+            </span>
+            <span class="additional">
+                トレンドトピック: 
+                <a href="#">緊急手術</a>、
+                <a href="#">硬膜外血腫</a>
+            </span>
+        </div>
+        <div class="option">
+            ...
+        </div>
+    </div>
+    <div id="more">
+        <span>さらに表示</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    a {
+        text-decoration: none;
+        color: rgb(29, 155, 240);
+    }
+    a:hover{
+        text-decoration: underline;
+    }
+    #twitter-trend-container{
+        height: auto;
+        max-width: 400px;
+        min-width: 250px;
+        background-color: rgb(247, 249, 249);
+        border-radius: 15px;
+        padding: 10px 15px;
+        text-align: left;
+    }
+    #twitter-trend-container > h2{
+        font-size: 1.2em;
+        margin: 0px;
+        padding: 0px;
+    }
+
+    #twitter-trend-container > .trend {
+        display: flex;
+        flex-direction: row;
+        gap: 0px 12px;
+        margin: 15px 0px;
+    }
+    #twitter-trend-container > .trend > .content {
+        flex: 1;
+    }
+
+    #twitter-trend-container > .trend > .thumbnail {
+        width: 68px;
+    }
+
+
+    #twitter-trend-container > .trend > .content > .category{
+        display: block;
+        font-size: 0.8em;
+    }
+    #twitter-trend-container > .trend > .content > .title{
+        display: block;
+        font-size: 1em;
+        font-weight: bold;
+    }
+    #twitter-trend-container > .trend > .content > .additional{
+        display: block;
+        font-size: 0.8em;
+    }
+    #twitter-trend-container > .trend > .thumbnail > img{
+        width: 68px;
+        height: 68px;
+        object-fit: cover;
+        border-radius: 10px;
+    }
+    #twitter-trend-container > #more {
+        font-size: 0.9em;
+        color: rgb(29, 155, 240);
+    }
+</style>
