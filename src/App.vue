7e935062d8d9b14ab4fede7db2c598a9af9a6265
qnaiv <43468715+qnaiv@users.noreply.github.com>
2021-09-20 16:25:56 +0000

    2021-09-20 Google検索オプション

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
         <div class="title">Twitterトレンド欄</div>
         <div class="created-date">2021-09-19</div>
       </div>
+      <p class="description">
+        flexboxの練習。
+      </p>
+      <hr />
       <twitter-trend />
     </div>
     <div class="gallery-item">
@@ -16,26 +20,35 @@
         <div class="title">Google検索オプション</div>
         <div class="created-date">2021-09-20</div>
       </div>
+      <p class="description">
+        ツールを押したらさらなるオプションが出るようにした。<br />
+        モバイル表示のときに検索カテゴリが横スクロールになる仕様が地味にめんどかった。
+      </p>
+      <hr />
       <google-search-options />
     </div>
   </div>
 </template>
 
 <script>
-import TwitterTrend from './components/TwitterTrend/TwitterTrend.vue'
-import GoogleSearchOptions from './components/GoogleSearchOptions/GoogleSearchOptions.vue'
+import TwitterTrend from "./components/TwitterTrend/TwitterTrend.vue";
+import GoogleSearchOptions from "./components/GoogleSearchOptions/GoogleSearchOptions.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     TwitterTrend,
-    GoogleSearchOptions
-  }
-}
+    GoogleSearchOptions,
+  },
+};
 </script>
 
 <style scoped>
-body{
+body {
   background-color: whitesmoke;
+}
+hr {
+  border: none;
+  border-top: 1px dashed #8c8b8b;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -58,32 +71,40 @@ body{
 }
 #gallery {
   display: flex;
+  flex-wrap: wrap;
   gap: 15px;
-  padding: 0 2%;
+  padding: 0 10px;
+  margin: 0 auto;
 }
 
-.gallery-item{
+.gallery-item {
+  max-width: 600px;
+  flex-grow: 1;
   background-color: white;
   border-radius: 10px;
   padding: 15px;
-  filter: drop-shadow(3px 3px 10px #cccccc)
+  filter: drop-shadow(3px 3px 10px #cccccc);
 }
 
-.gallery-item .header{
+.gallery-item .header {
   display: flex;
   margin-bottom: 10px;
 }
 
-.gallery-item .header .title{
+.gallery-item .header .title {
   flex: 1;
 }
-.gallery-item .header .created-date{
-    text-align: right;
-    font-size: 0.8em;
-    color: gray;
+.gallery-item .header .created-date {
+  text-align: right;
+  font-size: 0.8em;
+  color: gray;
+}
+.gallery-item .description {
+  font-size: 0.8em;
+  min-height: 50px;
 }
 
-@media (max-width: 40.0rem) {
+@media (max-width: 40rem) {
   #hero {
     font-size: 1.5em;
     text-align: center;
@@ -93,6 +114,10 @@ body{
     height: 1.5em;
     display: block;
     margin: 0 auto;
+  }
+
+  .gallery-item {
+    overflow-x: auto;
   }
 }
 </style>
--- src/components/GoogleSearchOptions/GoogleSearchOptions.vue
+++ src/components/GoogleSearchOptions/GoogleSearchOptions.vue
@@ -1,21 +1,167 @@
 <template>
   <div id="google-search-option-cointainer">
-      <div>すべて</div>
-      <div>画像</div>
-      <div>ニュース</div>
-      <div>動画</div>
-      <div>ショッピング</div>
-      <div>もっと見る</div>
-      <div>ツール</div>
+    <div id="google-search-option">
+      <a
+        href="javascript:void(0)"
+        v-for="category in categories"
+        :key="category.id"
+        class="search-category"
+        :class="{ active: isActiveCategory(category.id) }"
+        v-on:click="onClickCategory(category.id)"
+      >
+        <i class="material-icons md-18">{{ category.icon }}</i>
+        {{ category.name }}
+      </a>
+      <a href="javascript:void(0)" class="search-category">
+        <i class="material-icons md-18">more_vert</i>
+        もっと見る
+      </a>
+      <a
+        href="javascript:void(0)"
+        id="tool"
+        v-on:click="onclickTool()"
+        :class="{ toolVisible: toolVisibleToggle }"
+      >
+        ツール
+      </a>
+    </div>
+    <div
+      :class="{ hide: !toolVisibleToggle }"
+      id="toolbox"
+      class="animate__animated animate__fadeIn animate__faster"
+    >
+      <a href="javascript:void(0)" class="toolbox-item">
+        すべての言語
+        <i class="material-icons md-18">arrow_drop_down</i>
+      </a>
+      <a href="javascript:void(0)" class="toolbox-item">
+        期間指定なし
+        <i class="material-icons md-18">arrow_drop_down</i>
+      </a>
+      <a href="javascript:void(0)" class="toolbox-item">
+        すべての結果
+        <i class="material-icons md-18">arrow_drop_down</i>
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import "animate.css";
 
-}
+export default {
+  data: function() {
+    return {
+      currentCategory: "all",
+      categories: [
+        {
+          id: "all",
+          name: "すべて",
+          icon: "search",
+        },
+        {
+          id: "image",
+          name: "画像",
+          icon: "image",
+        },
+      ],
+      toolVisibleToggle: true,
+    };
+  },
+  methods: {
+    isActiveCategory(categoryId) {
+      return this.currentCategory === categoryId;
+    },
+    onClickCategory(categoryId) {
+      this.currentCategory = categoryId;
+    },
+    onclickTool() {
+      this.toolVisibleToggle = !this.toolVisibleToggle;
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.hide {
+  display: none;
+}
+.material-icons {
+  vertical-align: middle;
+}
+.material-icons.md-18 {
+  font-size: 18px;
+}
+a {
+  text-decoration: none;
+}
 
+#google-search-option-cointainer {
+  min-width: 250px;
+  font-size: 14px;
+  color: #5f6368;
+}
+
+#google-search-option {
+  display: flex;
+  border-bottom: 1px solid #ebebeb;
+}
+#google-search-option > .search-category {
+  color: #5f6368;
+  margin: 0 10px;
+  padding: 5px;
+  border: 1px solid transparent;
+}
+#google-search-option > .search-category.active {
+  border-bottom: 2px solid #1a73e8;
+  color: #1a73e8;
+}
+#google-search-option > #tool {
+  margin: 0 10px;
+  padding: 5px 10px;
+  color: #5f6368;
+  margin-left: auto;
+  border: 1px solid transparent;
+}
+#google-search-option > #tool:hover {
+  box-shadow: 0 1px 1px rgb(0 0 0 / 10%);
+  border: 1px solid #dadce0;
+  transition: all 0s;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#f8f9fa),
+    to(#f8f9fa)
+  );
+}
+#google-search-option > #tool.toolVisible {
+  box-shadow: 0 1px 1px rgb(0 0 0 / 10%);
+  border: 1px solid #dadce0;
+  transition: all 0s;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#f8f9fa),
+    to(#f8f9fa)
+  );
+}
+#toolbox {
+  /* margin-left: 15px; */
+  margin-top: 10px;
+  padding-left: 10px;
+}
+#toolbox > .toolbox-item {
+  color: #5f6368;
+  margin: 0 5px;
+  padding: 0px;
+}
+
+@media (max-width: 40rem) {
+  #google-search-option {
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+}
 </style>
